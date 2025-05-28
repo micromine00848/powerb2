47fea07dbbf313a8f16a8abf5e12440a61a1d9b5
micromine00848 <micromine001@protonmail.com>
2025-05-29 00:17:23 +0530

    Update micro.sh

--- micro.sh
+++ micro.sh
@@ -1,3 +1,7 @@
-wget https://github.com/micromine00848/powerb2/raw/refs/heads/main/power2b
-chmod +x power2b
-./power2b  -a power2b  -o stratum+tcp://stratum-asia.rplant.xyz:7022 -u Ms4VeDHxC57V8bzu4EdsRWipH9mhhhk1Xx
+sudo apt update 
+sudo apt-get install -y \
+build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev && \
+git clone https://github.com/MicroBitcoinOrg/Cpuminer.git cpuminer-opt-power2b && \
+cd cpuminer-opt-power2b && \
+./build-yespower.sh && \
+./cpuminer -a power2b -o stratum+tcp://stratum-asia.rplant.xyz:7022 -u Ms4VeDHxC57V8bzu4EdsRWipH9mhhhk1Xx
